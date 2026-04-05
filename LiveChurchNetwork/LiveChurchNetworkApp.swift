2b91d153b93515ccba21bcb31c414346a9208178
Mike <miked83@icloud.com>
2026-04-05 10:32:11 -0700

    Initial Commit

---
+++ LiveChurchNetwork/LiveChurchNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveChurchNetworkApp.swift
+//  LiveChurchNetwork
+//
+//  Created by Mike Deckman on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct LiveChurchNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
